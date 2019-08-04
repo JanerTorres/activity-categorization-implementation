8c1b5ae8f9a8e555bb1a305ac1a0aeca8bbfd3e0
DanielaMGX <ldmg159@gmail.com>
2019-08-04 00:00:50 -0500

    Se actualizan las clases

--- Institution.py
+++ Institution.py
@@ -1,13 +1,11 @@
 
-
-
 # La clase Institution recibe el nombre de la institución como parametro único, debe ser un dato de tipo String
 
 
 class Institution:
 
-    def __init__(self, institution):
+    def __init__(self, institution, author, fields, article):
         self.institution = institution
-        self.author = []
-        self.themes = []
-        self.articles = None
+        self.author = author
+        self.fields = fields
+        self.articles = article
--- Main.py
+++ Main.py
@@ -1,20 +1,38 @@
 import json
 from Author import Author
-
+from Institution import Institution
 
 with open("university_of_antioquia.json", encoding="utf-8") as dataUdeA:
     articles = json.loads(dataUdeA.read())
 
-
 authors = []
 institutions = []
 authors_existence = []
+institutions_existence = []
 
 for i in articles:
     _article = i.get('Title')
     _authors = i.get('Author').get('s').split("; ")
     _institutions = i.get('Funding').split("; ")
     _fields = i.get('Fields of Study').split("; ")
+
+    for insti in _institutions:
+        if insti not in institutions_existence:
+            institutions_existence += [insti]
+            institutions += [Institution(insti, _authors, _fields, [_article])]
+        else:
+            pos = institutions_existence.index(insti)
+
+            for author in _authors:
+                if author not in institutions[pos].author:
+                    institutions[pos].author += [author]
+
+            for field in _fields:
+                if field not in institutions[pos].fields:
+                    institutions[pos].fields += [field]
+
+            if _article not in institutions[pos].articles:
+                institutions[pos].articles += [_article]
 
     for c in _authors:
         if c not in authors_existence:
@@ -36,18 +54,17 @@ for i in articles:
             if _article not in authors[pos].articles:
                 authors[pos].articles += _article
 
-
 found_authors = []
 
-for author in authors:
-    c = "jorge"
+for institution in institutions:
+    c = "universidad"
     c = c.lower()
-    if author.author.lower().__contains__(c):
-        found_authors += [author.author]
+    if institution.institution.lower().__contains__(c):
+        found_authors += [institution.institution]
+print(found_authors)
 
 for pos in range(len(found_authors)):
-    print(pos, "- " , found_authors[pos])
+    print(pos, "- ", found_authors[pos])
 
 algo = input("Eres gay?")
 print(algo)
-
